971cb8087c424138b1256dad02512d03545fa626
Jhamere Wilson <jhamerewilson@outlook.com>
2021-05-26 15:13:01 -0400

    Added iRepo Interface to Service Container

--- src/CommandApi/Controllers/CommandsController.cs
+++ src/CommandApi/Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandApi.Data;
+using CommandApi.Models;
 
 namespace CommandApi.Controllers
 {
@@ -7,10 +9,28 @@ namespace CommandApi.Controllers
     [Route("api/[controller]")]
     public class CommandsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly ICommandApiRepo _repository;
+        public CommandsController(ICommandApiRepo repository)
         {
-            return new string[] { "this", "is", "hard", "coded" };
+            _repository = repository;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems); // returns http 200 result code and passes back result json command data
+        }
+        [HttpGet]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
         }
     }
+
+
 }
