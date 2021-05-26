2dbb4f5f5d506b6c5fcee63d4f3e922e22fbaff9
Jhamere Wilson <jhamerewilson@outlook.com>
2021-05-26 13:34:12 -0400

    Initial Commit

---
+++ src/CommandApi/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+    }
+}
